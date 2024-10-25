b12054eb35774f8b05512dd2158e0a8d7c7fd4fb
Marina Kostova <marinakostova9222@gmail.com>
2024-10-25 19:44:47 +0000

    Added guinea pigs template , updated views and urls

--- adoptions/views.py
+++ adoptions/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import GuineaPig
 
 # Create your views here.
+
+def guinea_pigs(request):
+    """ A view to show all guinea pigs including sorting"""
+
+    guinea_pigs = GuineaPig.objects.all()
+    
+    context = {
+        'guinea_pigs':guinea_pigs,
+    }
+
+
+    return render(request, '/templates/guinea_pigs.html', context)
--- little_wheekers_rescue/urls.py
+++ little_wheekers_rescue/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('guinea_pigs/', include('guinea_pigs.urls')),
    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
