eec08606338626a6b295746b45cbba6e0a3654c6
Marina Kostova <marinakostova9222@gmail.com>
2024-10-26 13:27:01 +0000

    Added template for about us page , added views , urls for the about us page

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'home' 
+
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='index'),  
+    path('about/', views.about_us, name='about_us'),  
 ]
--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    """ A view to return the index page """
+    return render(request, 'home/index.html') 
 
-    return render(request, 'home/index.html')
+def about_us(request):
+    return render(request, 'home/about_us.html')
