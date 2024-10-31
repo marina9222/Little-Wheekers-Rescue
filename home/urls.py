f70c42686cc039fd2651726e860b44c83ca40183
Marina Kostova <marinakostova9222@gmail.com>
2024-10-31 14:43:34 +0000

    Set up stripe payment for donations and created templates for the donation

--- home/urls.py
+++ home/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('about/', views.about_us, name='about_us'),
     path('adoption-policy/', views.adoption_policy, name='adoption_policy'),
     path('', home_view, name='home'),
+    path('donate/', views.donate, name='donate'),
+    path('donate/success/', views.donation_success, name='donation_success'),
+    path('donate/cancel/', views.donation_cancel, name='donation_cancel'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import stripe
 from django.conf import settings
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
@@ -18,3 +20,34 @@ def adoption_policy(request):
 
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
+
+@csrf_exempt
+def donate(request):
+    if request.method == 'POST':
+        amount = int(request.POST.get('amount')) * 100  
+
+        session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=[{
+                'price_data': {
+                    'currency': 'gbp',
+                    'product_data': {
+                        'name': 'Donation to Little Wheekers Rescue',
+                    },
+                    'unit_amount': amount,
+                },
+                'quantity': 1,
+            }],
+            mode='payment',
+            success_url=request.build_absolute_uri(reverse('home:donation_success')),
+            cancel_url=request.build_absolute_uri(reverse('home:donation_cancel')),
+        )
+        return redirect(session.url, code=303)
+
+    return render(request, 'home/donate.html')
+
+def donation_success(request):
+    return render(request, 'home/donation_success.html')
+
+def donation_cancel(request):
+    return render(request, 'home/donation_cancel.html')
