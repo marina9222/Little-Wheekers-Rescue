e1a21fcebee0a561fef84340fa28472dfc55df45
Marina Kostova <marinakostova9222@gmail.com>
2024-11-22 21:43:39 +0000

    Fix : adopt guinea pig form changed to django form

--- adoptions/forms.py
+++ adoptions/forms.py
@@ -6,3 +6,10 @@ class AdoptionForm(forms.ModelForm):
     class Meta:
         model = Adoption
         exclude = ['guinea_pig', 'adopter', 'created_at']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({
+                'class': 'form-control form-control-lg',
+            })
