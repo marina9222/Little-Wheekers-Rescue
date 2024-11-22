de1634f2492a903d9d93f343acdfc0999a46db6a
Marina Kostova <marinakostova9222@gmail.com>
2024-11-22 21:15:37 +0000

    Fix : form so the min and max guinea pig count on adoption form to work

--- adoptions/forms.py
+++ adoptions/forms.py
@@ -1,10 +1,8 @@
 from django import forms
+from .models import Adoption
 
 
-class AdoptionForm(forms.Form):
-    address = forms.CharField(max_length=255)
-    phone_number = forms.CharField(max_length=15)
-    number_of_guinea_pigs = forms.IntegerField()
-    living_arrangement = forms.ChoiceField(
-        choices=[('flat', 'Flat'), ('house', 'House')]
-    )
+class AdoptionForm(forms.ModelForm):
+    class Meta:
+        model = Adoption
+        exclude = ['guinea_pig', 'adopter', 'created_at']
