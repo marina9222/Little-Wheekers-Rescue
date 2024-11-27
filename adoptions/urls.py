f6e51407e16a271f33095836a94bc9af4724f2ad
Marina Kostova <marinakostova9222@gmail.com>
2024-11-27 21:40:29 +0000

    Fix : indentation

--- adoptions/urls.py
+++ adoptions/urls.py
@@ -1,13 +1,34 @@
 from django.urls import path
 from . import views
-from .views import available_guinea_pigs, adopt_guinea_pig, adoption_success, my_profile
+from .views import (
+    available_guinea_pigs,
+    adopt_guinea_pig,
+    adoption_success,
+    my_profile
+)
 
 app_name = 'adoptions'
 
-
 urlpatterns = [
-    path('available/', available_guinea_pigs, name='available_guinea_pigs'),  
-    path('<int:guinea_pig_id>/adopt/', adopt_guinea_pig, name='adopt_guinea_pig'),
-    path('success/', adoption_success, name='adoption_success'),
-    path('profile/', my_profile, name='my_profile'),
-    ]
+    path(
+        'available/',
+        available_guinea_pigs,
+        name='available_guinea_pigs'
+    ),
+    path(
+        '<int:guinea_pig_id>/adopt/',
+        adopt_guinea_pig,
+        name='adopt_guinea_pig'
+    ),
+    path(
+        'success/',
+        adoption_success,
+        name='adoption_success'
+    ),
+    path(
+        'profile/',
+        my_profile,
+        name='my_profile'
+    ),
+]
+
