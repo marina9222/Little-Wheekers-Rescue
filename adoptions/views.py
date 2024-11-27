1cefdd6eaa6f13e2dc28c9b9e7934534558f2dad
Marina Kostova <marinakostova9222@gmail.com>
2024-11-27 21:51:59 +0000

    Fix: indentation

--- adoptions/views.py
+++ adoptions/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import GuineaPig, Adoption 
+from .models import GuineaPig, Adoption
 from home.models import Donation
 from django.contrib.auth.decorators import login_required
 from django.db.models import F
@@ -7,41 +7,41 @@ from .forms import AdoptionForm
 from django.core.mail import send_mail
 from django.conf import settings
 
+
 # Create your views here.
 
 # View for displaying available guinea pigs for adoption
 def available_guinea_pigs(request):
     guinea_pigs = GuineaPig.objects.all().order_by("adopted", "name")
-    
-    
-    sort_by = request.GET.get('sort', None) 
+
+    sort_by = request.GET.get('sort', None)
 
     if sort_by == 'age_asc':
-        guinea_pigs = guinea_pigs.order_by('-birth_year')  
+        guinea_pigs = guinea_pigs.order_by('-birth_year')
     elif sort_by == 'age_desc':
         guinea_pigs = guinea_pigs.order_by('birth_year')
     elif sort_by == 'gender_male':
         guinea_pigs = guinea_pigs.filter(gender='M')
     elif sort_by == 'gender_female':
         guinea_pigs = guinea_pigs.filter(gender='F')
-    
 
     return render(request, 'adoptions/available_guinea_pigs.html', {
         'guinea_pigs': guinea_pigs,
         'selected_sort': sort_by,
     })
 
-    
+
 @login_required
 def my_profile(request):
     # Fetch adoptions and donations for the authenticated user
     adoptions = Adoption.objects.filter(adopter=request.user)
-    donations = Donation.objects.filter(user=request.user).order_by('-date')  # Assuming 'date' is a field in the Donation model
+    donations = Donation.objects.filter(user=request.user).order_by('-date')
 
-    return render(request, 'adoptions/my_profile.html', { 
+    return render(request, 'adoptions/my_profile.html', {
         'adoptions': adoptions,
-        'donations': donations,  
+        'donations': donations,
     })
+
 
 # View for adopting a specific guinea pig
 @login_required
@@ -50,25 +50,26 @@ def adopt_guinea_pig(request, guinea_pig_id):
 
     if request.method == 'POST':
         form = AdoptionForm(request.POST)
-        if form.is_valid():  
-            
+        if form.is_valid():
+
             adoption = Adoption(
                 guinea_pig=guinea_pig,
                 adopter=request.user,
                 address=form.cleaned_data['address'],
                 phone_number=form.cleaned_data['phone_number'],
-                number_of_guinea_pigs=form.cleaned_data['number_of_guinea_pigs'],
+                number_of_guinea_pigs=form.cleaned_data
+                ['number_of_guinea_pigs'],
                 living_arrangement=form.cleaned_data['living_arrangement'],
             )
             adoption.save()
             guinea_pig.adopted = True
-            guinea_pig.save()  
+            guinea_pig.save()
 
             user_email = request.user.email
             subject = f"Thank you for submitting your adoption form!"
             message = (
                 f"Hi {request.user.first_name},\n\n"
-                f"Thank you for submitting your adoption form for {guinea_pig.name}. "
+                f"Thank you for submitting your adoption form for {guinea_pig.name}."
                 "We are excited to process your application and will get back to you soon.\n\n"
                 "Best regards,\nThe Little Wheekers Rescue Team"
             )
@@ -85,9 +86,11 @@ def adopt_guinea_pig(request, guinea_pig_id):
     else:
         form = AdoptionForm()
 
-    return render(request, 'adoptions/adopt_guinea_pig.html', {'guinea_pig': guinea_pig, 'form': form})
+    return render(request, 'adoptions/adopt_guinea_pig.html', {
+        'guinea_pig': guinea_pig,
+        'form': form,
+    })
+
 
 def adoption_success(request):
     return render(request, 'adoptions/adoption_success.html')
-
-
