ea168d092d5024c679267789f9945120606a897c
Marina Kostova <marinakostova9222@gmail.com>
2024-11-28 20:52:33 +0000

    Fix: indentation

--- manage.py
+++ manage.py
@@ -6,7 +6,9 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'little_wheekers_rescue.settings')
+    os.environ.setdefault(
+        'DJANGO_SETTINGS_MODULE', 'little_wheekers_rescue.settings'
+    )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
