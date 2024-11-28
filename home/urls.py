c7790b856b960a404d40f61553e9009771ea454b
Marina Kostova <marinakostova9222@gmail.com>
2024-11-28 20:49:58 +0000

    Fix: indentation

--- home/urls.py
+++ home/urls.py
@@ -3,10 +3,9 @@ from django.urls import path
 from . import views
 from home.views import home_view
 
+app_name = 'home'
 
-app_name = 'home' 
-
-urlpatterns = [ 
+urlpatterns = [
     path('about/', views.about_us, name='about_us'),
     path('adoption-policy/', views.adoption_policy, name='adoption_policy'),
     path('', home_view, name='home'),
@@ -14,6 +13,14 @@ urlpatterns = [
     path('donate/success/', views.donation_success, name='donation_success'),
     path('donate/cancel/', views.donation_cancel, name='donation_cancel'),
     path('management/', views.management_view, name='management'),
-    path('guinea-pig/<int:guinea_pig_id>/edit/', views.edit_guinea_pig, name='edit_guinea_pig'),
-    path('guinea-pig/<int:guinea_pig_id>/delete/', views.delete_guinea_pig, name='delete_guinea_pig'),
+    path(
+        'guinea-pig/<int:guinea_pig_id>/edit/',
+        views.edit_guinea_pig,
+        name='edit_guinea_pig'
+    ),
+    path(
+        'guinea-pig/<int:guinea_pig_id>/delete/',
+        views.delete_guinea_pig,
+        name='delete_guinea_pig'
+    ),
 ]
